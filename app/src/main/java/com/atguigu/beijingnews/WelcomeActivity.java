c2ff281488b4324d72166c9fa5ef5d98b08e4f9d
z920393030 <9203930303@qq.com>
2017-06-02 08:49:15 +0800

    欢迎页面完成

---
+++ app/src/main/java/com/atguigu/beijingnews/WelcomeActivity.java
@@ -0,0 +1,63 @@
+package com.atguigu.beijingnews;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.RotateAnimation;
+import android.view.animation.ScaleAnimation;
+import android.widget.RelativeLayout;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    private RelativeLayout activity_welcome;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+        activity_welcome = (RelativeLayout)findViewById(R.id.activity_welcome);
+
+
+        RotateAnimation ra  = new RotateAnimation(0,360,RotateAnimation.RELATIVE_TO_SELF,0.5f,RotateAnimation.RELATIVE_TO_SELF,0.5f);
+        ra.setDuration(2000);
+        ra.setFillAfter(true);
+
+        AlphaAnimation aa = new AlphaAnimation(0,1);
+        aa.setDuration(2000);
+        aa.setFillAfter(true);
+
+        ScaleAnimation sa = new ScaleAnimation(0,1,0,1,ScaleAnimation.RELATIVE_TO_SELF,0.5f,ScaleAnimation.RELATIVE_TO_SELF,0.5f);
+        sa.setDuration(2000);
+        sa.setFillAfter(true);
+
+        AnimationSet set = new AnimationSet(false);
+        set.addAnimation(ra);
+        set.addAnimation(aa);
+        set.addAnimation(sa);
+
+        activity_welcome.startAnimation(set);
+
+        set.setAnimationListener( new MyAnimationListener());
+
+    }
+
+    class MyAnimationListener implements Animation.AnimationListener{
+
+        @Override
+        public void onAnimationStart(Animation animation) {
+
+        }
+
+        @Override
+        public void onAnimationEnd(Animation animation) {
+
+        }
+
+        @Override
+        public void onAnimationRepeat(Animation animation) {
+
+        }
+    }
+
+}
