b0bcc00a936cb34fb9927a71d31a2bf7236df680
z920393030 <9203930303@qq.com>
2017-06-03 11:50:21 +0800

    左侧菜单数据绑定&状态处理

--- app/src/main/java/com/atguigu/beijingnews/fragment/LeftMenuFragment.java
+++ app/src/main/java/com/atguigu/beijingnews/fragment/LeftMenuFragment.java
@@ -1,11 +1,14 @@
 package com.atguigu.beijingnews.fragment;
 
-import android.graphics.Color;
-import android.util.Log;
-import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import com.atguigu.beijingnews.MainActivity;
+import com.atguigu.beijingnews.R;
 import com.atguigu.beijingnews.domain.NewsCenterBean;
 
 import java.util.List;
@@ -15,28 +18,66 @@ import java.util.List;
  */
 
 public class LeftMenuFragment extends BaseFragment {
-    private TextView textView;
+    private ListView listView;
     List<NewsCenterBean.DataBean> datas;
+    private int prePosition = 0;
+    private LeftMenuAdapter adapter;
 
     @Override
     public View initView() {
-        textView = new TextView(context);
-        textView.setTextSize(25);
-        textView.setGravity(Gravity.CENTER);
-        textView.setTextColor(Color.RED);
-        return textView;
+        listView = new ListView(context);
+        listView.setPadding(0,40,0,0);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                prePosition = position;
+                adapter.notifyDataSetChanged();
+                MainActivity mainActivity = (MainActivity) context;
+                mainActivity.getSlidingMenu().toggle();
+            }
+        });
+        return listView;
     }
 
     @Override
     public void initData() {
         super.initData();
-        textView.setText("左侧菜单");
     }
 
     public void setData(List<NewsCenterBean.DataBean> datas) {
         this.datas = datas;
-        for(int i = 0; i < datas.size(); i++) {
-            Log.e("TAG",""+datas.get(i).getTitle());
+        adapter = new LeftMenuAdapter();
+        listView.setAdapter(adapter);
+    }
+
+    private class LeftMenuAdapter extends BaseAdapter{
+        @Override
+        public int getCount() {
+            return datas==null?0:datas.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            TextView textView = (TextView) View.inflate(context, R.layout.item_leftmenu,null);
+            if(prePosition==position){
+                textView.setEnabled(true);
+            }else {
+                textView.setEnabled(false);
+            }
+
+            NewsCenterBean.DataBean dataBean = datas.get(position);
+            textView.setText(dataBean.getTitle());
+            return textView;
         }
     }
 }
