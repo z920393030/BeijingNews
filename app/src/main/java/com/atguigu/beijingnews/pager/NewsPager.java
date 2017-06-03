b8d3165082b92c067ff479ce1a748508c9251f13
z920393030 <9203930303@qq.com>
2017-06-03 11:06:25 +0800

    新闻中心数据请求&json数据解析

--- app/src/main/java/com/atguigu/beijingnews/pager/NewsPager.java
+++ app/src/main/java/com/atguigu/beijingnews/pager/NewsPager.java
@@ -2,8 +2,17 @@ package com.atguigu.beijingnews.pager;
 
 import android.content.Context;
 import android.graphics.Color;
+import android.util.Log;
 import android.view.Gravity;
 import android.widget.TextView;
+
+import com.atguigu.beijingnews.domain.NewsCenterBean;
+import com.atguigu.beijingnews.utils.ConstantUtils;
+import com.google.gson.Gson;
+import com.zhy.http.okhttp.OkHttpUtils;
+import com.zhy.http.okhttp.callback.StringCallback;
+
+import okhttp3.Call;
 
 /**
  * Created by My on 2017/6/2.
@@ -26,5 +35,34 @@ public class NewsPager extends BasePager {
         textView.setTextColor(Color.RED);
 
         fl_content.addView(textView);
+
+        getDataFromNet();
+    }
+
+    private void getDataFromNet() {
+        String url = ConstantUtils.NEWSCENTER_PAGER_URL;
+
+        OkHttpUtils
+                .get()
+                .url(url)
+                .build()
+                .execute(new StringCallback() {
+
+                    @Override
+                    public void onError(Call call, Exception e, int id) {
+                        Log.e("TAG","请求失败=="+e.getMessage());
+                    }
+
+                    @Override
+                    public void onResponse(String response, int id) {
+                        Log.e("TAG","请求成功=="+response);
+                        processData(response);
+                    }
+                });
+    }
+
+    private void processData(String json) {
+        NewsCenterBean newsCenterBean = new Gson().fromJson(json,NewsCenterBean.class);
+        Log.e("TAG","解析成功了哦=="+ newsCenterBean.getData().get(0).getChildren().get(0).getTitle());
     }
 }
---
+++ app/src/main/java/com/atguigu/beijingnews/utils/ConstantUtils.java
@@ -0,0 +1,11 @@
+package com.atguigu.beijingnews.utils;
+
+/**
+ * Created by My on 2017/6/3.
+ */
+
+public class ConstantUtils {
+    public static final String BASE_URL = "http://182.92.5.3/web_home";
+
+    public static final String NEWSCENTER_PAGER_URL = BASE_URL + "/static/api/news/categories.json";
+}
